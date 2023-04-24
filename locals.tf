c7bc2febbf72797d9f693ef299aec9b5529c0408
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-24 16:54:01 +0530

    condition in job

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  common_tags={
+    env           =var.env
+    project       = "roboshop1"
+    business_unit = "ecommerce"
+    owner         = "ecommerce-robot"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
 resource "aws_vpc" "dev" {
   cidr_block = var.cidr_block
+  tags       = local.common_tags
 }
