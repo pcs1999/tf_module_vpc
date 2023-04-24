bf72f76c8978034a18e8dabc928e7335d7977b66
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-24 20:18:09 +0530

    subnet_creation

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "aws_vpc" "dev" {
   cidr_block = var.cidr_block
   tags       = merge (local.common_tags, { Name = "${var.env}-vpc"})
 }
+
+
+resource "aws_subnet" "main-dev" {
+  vpc_id     = aws_vpc.dev.id
+  cidr_block = var.cidr_block
+
+  tags = merge (local.common_tags, { Name = "${var.env}-subnet"})
+
+}
