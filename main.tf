2644de5bccdab0d48b965a04ab26bb9246c62100
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-25 13:12:36 +0530

    vpc_peering_tag

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "dev" {
   cidr_block = var.cidr_block
-  tags       = merge (local.common_tags, { Name = "${var.env}-vpc"})
+  tags       = merge (local.common_tags, { Name = "${var.env}-VPC"})
 }
 
 
@@ -9,7 +9,7 @@ resource "aws_subnet" "main-dev" {
   vpc_id     = aws_vpc.dev.id
   cidr_block = var.subnets_cidr[count.index]
 
-  tags = merge (local.common_tags, { Name = "${var.env}-subnet{count.index+2}"})
+  tags = merge (local.common_tags, { Name = "${var.env}-subnet-${count.index + 2}" } )
 
 }
 
@@ -26,6 +26,13 @@ resource "aws_vpc_peering_connection" "foo" {
 resource "aws_route" "default" {
   route_table_id            = aws_vpc.dev.default_route_table_id
   destination_cidr_block    = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
 
 }
