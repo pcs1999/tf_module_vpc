22ef63a60942a76bfddf5ad604c427c40ff3b4c8
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-25 19:08:58 +0530

    creating_ec2_under_private_vpc

--- main.tf
+++ main.tf
@@ -47,3 +47,54 @@ resource "aws_internet_gateway" "gw" {
 
 }
 
+//create  EC2 instance
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+
+resource "aws_instance" "firstec2" {
+  ami = data.aws_ami.example.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id              = aws_subnet.main-dev.*.id[0]
+
+  tags = {
+    Name= "automachine"
+  }
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id = aws_vpc.dev.id
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
