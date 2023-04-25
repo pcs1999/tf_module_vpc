06a260abf42429300e897dab26fa5ee6807d64d9
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-25 11:59:15 +0530

    vpc_peering

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ resource "aws_subnet" "main-dev" {
   vpc_id     = aws_vpc.dev.id
   cidr_block = var.subnets_cidr[count.index]
 
-  tags = merge (local.common_tags, { Name = "${var.env}-subnet"})
+  tags = merge (local.common_tags, { Name = "${var.env}-subnet{count.index+2}"})
 
+}
+
+resource "aws_vpc_peering_connection" "foo" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = "vpc-0738d5702c63820c7"
+  vpc_id        = aws_vpc.dev.id
 }
