16133ef7ee09015b3084d09953e9d03777fff1e6
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-25 17:21:05 +0530

    creating_ec2_under_private_vpc

--- main.tf
+++ main.tf
@@ -39,6 +39,66 @@ resource "aws_route" "default-vpc" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.dev.id
 
-  tags = merge (local.common_tags, { Name = "${var.env}-igw}" } )
+  tags = merge (local.common_tags, { Name = "${var.env}-igw" } )
 
+}
+
+//create ec2
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id = aws_subnet.main-dev.id
+
+  tags = {
+    Name = "test-centos8"
+  }
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-b70"
+    key    = "05-remote-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+//security_groeup
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id = aws_vpc.dev.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
