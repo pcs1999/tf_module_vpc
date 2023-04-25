fcda02d95dbe6df35b4f88058de3cbb25dde5737
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-25 16:34:54 +0530

    igw

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "dev" {
   cidr_block = var.cidr_block
-  tags       = merge (local.common_tags, { Name = "${var.env}-VPC"})
+  tags       = merge (local.common_tags, { Name = "${var.env}-NVPC"})
 }
 
 
@@ -9,7 +9,7 @@ resource "aws_subnet" "main-dev" {
   vpc_id     = aws_vpc.dev.id
   cidr_block = var.subnets_cidr[count.index]
 
-  tags = merge (local.common_tags, { Name = "${var.env}-subnet-${count.index + 2}" } )
+  tags = merge (local.common_tags, { Name = "${var.env}-subnet-${count.index + 3}" } )
 
 }
 
@@ -17,9 +17,8 @@ resource "aws_vpc_peering_connection" "foo" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.dev.id
-  tags = {
-    Name = "${var.env}-peering"
-  }
+  tags = merge (local.common_tags, { Name = "${var.env}-subnet-${count.index + 3}" } )
+
   auto_accept = true
 }
 
@@ -34,5 +33,12 @@ resource "aws_route" "default-vpc" {
   route_table_id            = data.aws_vpc.default.main_route_table_id
   destination_cidr_block    = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge (local.common_tags, { Name = "${var.env}-subnet-${count.index + 2}" } )
 
 }
