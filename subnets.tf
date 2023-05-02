846379d3c773f81b44589d50fe1b35c56c3507b0
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-02 14:25:56 +0530

    separate subnets creation

--- subnets.tf
+++ subnets.tf
@@ -12,4 +12,9 @@ module "subnets" {
 
   vpc_id                    = aws_vpc.dev_vpc.id
   vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+
+  internet_gw = lookup(each.value, "internet-gw" , false )
+  nat_gw = lookup(each.value,"nat_gw",false )
+  create_nat_gw = lookup(each.value,"create_nat_gw",false )
+
 }
