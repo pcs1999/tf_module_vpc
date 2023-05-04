6371c709e12ffa89755f961388dd50bab2ad1a62
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-04 12:27:15 +0530

    public&private_subnets

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,28 @@
-module "subnets" {
+#module "subnets" {
+#  source                    = "./subnets"
+#
+#  for_each                  = var.subnets
+#
+#  env                       = var.env
+#  availability_zones        = var.availability_zones
+#  default_vpc_id            = var.default_vpc_id
+#
+#  name                      = each.value.name
+#  cidr_block                = each.value.cidr_block
+#
+#  vpc_id                    = aws_vpc.dev_vpc.id
+#  vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+#
+# internet_gw = lookup(each.value, "internet_gw" , false)
+##  nat_gw = lookup(each.value,"nat_gw",false )
+#  gateway_id   = aws_internet_gateway.igw.id
+#
+#}
+
+module "public_subnets" {
   source                    = "./subnets"
 
-  for_each                  = var.subnets
+  for_each                  = var.public_subnets
 
   env                       = var.env
   availability_zones        = var.availability_zones
@@ -13,8 +34,8 @@ module "subnets" {
   vpc_id                    = aws_vpc.dev_vpc.id
   vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
 
- internet_gw = lookup(each.value, "internet_gw" , false)
-#  nat_gw = lookup(each.value,"nat_gw",false )
+  internet_gw = lookup(each.value, "internet_gw" , false)
+  #  nat_gw = lookup(each.value,"nat_gw",false )
   gateway_id   = aws_internet_gateway.igw.id
 
 }
