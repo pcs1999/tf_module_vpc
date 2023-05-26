a60ded2f17dbc18f073b0955db24c3bea327106c
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-26 13:17:31 +0530

    changed to new account jenkins server

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   common_tags={
-    env           =var.env
+    env           =  var.env
     project       = "roboshop1"
     business_unit = "ecommerce"
     owner         = "ecommerce-robot"
